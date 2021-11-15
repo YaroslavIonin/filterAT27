53d2853d647fcb2329aecdcafad9c63f95ebf9d8
Ionin Yaroslav <ioninyaroslav22@gmail.com>
2021-11-15 22:12:32 +0500

    3 часть

--- filter.py
+++ filter.py
@@ -4,6 +4,18 @@ from itertools import product
 
 
 def get_middle(i, j):
+    """
+    Функция находит среднее значение вокруг заданного i, j, в зависимости от размеров мозайки
+    :param i: номер строчки пикселя
+    :param j: номер колонки пикселя
+    :return: среднее значение
+    >>>: get_middle(45, 2)
+    19.0
+    >>>:get_middle(58, 60)
+    22.0
+    >>>get_middle(235, 101)
+    214.0
+    """
     s = 0
     for n, n1 in product(range(i, i + sizeX), range(j, j + sizeY)):
         for a in range(0, len(matrix[int(n)][int(n1)])):
@@ -41,5 +53,6 @@ sizeX = int(input())
 print('Введите ширину мозайки')
 sizeY = int(input())
 print('Введите количество градаций серого')
-gradation = int(input())
+gradation = float(input())
 get_mosaic()
+
